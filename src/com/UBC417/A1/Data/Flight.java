beb930ff0ce7fcdeb8209b8170a393df6ab4ec41
Mark Graham Johnson <mgrahamjohnson@gmail.com>
2013-01-24 23:57:54 -0800

    Modified GetFlights() to account for the Flight Entities no longer containing the seats. Gets the Flight Entities, searches for the seats on each flight, then adds the seats to the flight Entities. Returns those flights in an ArrayList. Note that the flight Entities with the seats added are NOT put back into the data store.

--- src/com/UBC417/A1/Data/Flight.java
+++ src/com/UBC417/A1/Data/Flight.java
@@ -1,5 +1,8 @@
 package com.UBC417.A1.Data;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
@@ -9,7 +12,6 @@ import com.google.appengine.api.datastore.Transaction;
 
 //Flight Helper class, used to create and get flights from Google Datastore
 public class Flight {
-	public static final int NUM_SHARDS = 50;
 	
 	// Create a flight
 	// @store = true when you want to commit entity to datastore
@@ -28,7 +30,16 @@ public class Flight {
 	// Get all flights in the datastore
 	public static Iterable<Entity> GetFlights() {
 		DatastoreService ds = DatastoreServiceFactory.getDatastoreService();
-		Query q = new Query("Flight");
-		return ds.prepare(q).asIterable();
+		Query qFlight = new Query("Flight");
+		List<Entity> flights = new ArrayList<Entity>();
+		for(Entity flight : ds.prepare(qFlight).asIterable()){
+			
+			Query qSeat = new Query(flight.getKey().getName());
+			for(Entity seat : ds.prepare(qSeat).asIterable()){
+				flight.setPropertiesFrom(new Entity("Seat", seat.getKey().getName(), flight.getKey()));
+			}
+			flights.add(flight);
+		}
+		return flights;
 	}
 }
