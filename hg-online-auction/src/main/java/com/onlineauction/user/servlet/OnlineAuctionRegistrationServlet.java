a49ddf7c8685ed663ffd3c85aec7f2fadb41d37a
mgrahamjohnson <mgrahamjohnson@gmail.com>
2013-04-07 23:25:07 -0700

    hopefully I can push this...

--- hg-online-auction/src/main/java/com/onlineauction/user/servlet/OnlineAuctionRegistrationServlet.java
+++ hg-online-auction/src/main/java/com/onlineauction/user/servlet/OnlineAuctionRegistrationServlet.java
@@ -17,6 +17,8 @@
 package com.onlineauction.user.servlet;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.logging.Logger;
 
 import javax.servlet.ServletException;
@@ -25,6 +27,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import com.google.appengine.repackaged.com.google.common.base.StringUtil;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import com.onlineauction.user.domain.entity.User;
@@ -41,6 +44,16 @@ import com.onlineauction.user.name.domain.service.UserNameService;
 @SuppressWarnings("serial")
 @Singleton
 public class OnlineAuctionRegistrationServlet extends HttpServlet {
+	private static final String EMAIL = "email";
+
+	private static final String LAST_NAME = "lastName";
+
+	private static final String FIRST_NAME = "firstName";
+
+	private static final String PASSWORD = "password";
+
+	private static final String USER_NAME = "userName";
+
 	private static final Logger log = Logger
 			.getLogger(OnlineAuctionRegistrationServlet.class.getName());
 	
@@ -53,30 +66,54 @@ public class OnlineAuctionRegistrationServlet extends HttpServlet {
 	@Override
 	public void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException, ServletException {
-		String userName = req.getParameter("userName");
-		String password = req.getParameter("password");
+		Collection<String> missingFields = new ArrayList<String>();
+		String userName = req.getParameter(USER_NAME);
+		if (StringUtil.isEmptyOrWhitespace(userName)) {
+			missingFields.add(USER_NAME);
+		}
+		
+		String password = req.getParameter(PASSWORD);
+		if(StringUtil.isEmptyOrWhitespace(password)) {
+			missingFields.add(PASSWORD);
+		}
+		
 		String retypedPassword = req.getParameter("retypedPassword");
-		String firstName = req.getParameter("firstName");
-		String lastName = req.getParameter("lastName");
-		String email = req.getParameter("email");
+		
+		String firstName = req.getParameter(FIRST_NAME);
+		if (StringUtil.isEmptyOrWhitespace(firstName)) {
+			missingFields.add(FIRST_NAME);
+		}
+		
+		String lastName = req.getParameter(LAST_NAME);
+		if (StringUtil.isEmptyOrWhitespace(lastName)) {
+			missingFields.add(LAST_NAME);
+		}
+		
+		String email = req.getParameter(EMAIL);
+		if (StringUtil.isEmptyOrWhitespace(email)) {
+			missingFields.add(EMAIL);
+		}
+		
 		if(password.equals(retypedPassword)){
 			try{
 				
-				if (!userNameService.isUserNameValidForRegistration(userName)) {
+				if (!missingFields.isEmpty()) {
+					req.setAttribute("message", "Missing fields: " + missingFields.toString());
+					req.getRequestDispatcher("loginAndRegistration/failedRegister.jsp").forward(req, resp);
+				} else if (!userNameService.isUserNameValidForRegistration(userName)) {
 					req.setAttribute("message", "Username already taken");
 					req.getRequestDispatcher("loginAndRegistration/failedRegister.jsp").forward(req, resp);
-				} else {
-
+				} else { 
 					User newUser = new User(userName, UserType.BUYER, firstName, lastName, password, email);
 					userNameService.addUserName(userName);
 					userService.subscribeUser(newUser);
 					
 					log.info("Subscribed user with username: " + userName);
 					HttpSession session = req.getSession(true);
-					session.setAttribute("userName", newUser.getUserName());
-					session.setAttribute("firstName", newUser.getFirstName());
-					session.setAttribute("lastName", newUser.getLastName());
-					session.setAttribute("email", newUser.getEmail());
+					session.setAttribute(USER_NAME, newUser.getUserName());
+					session.setAttribute(FIRST_NAME, newUser.getFirstName());
+					session.setAttribute(LAST_NAME, newUser.getLastName());
+					session.setAttribute(EMAIL, newUser.getEmail());
 					req.getRequestDispatcher("home/auctionHome.jsp").forward(req, resp);
 				}
 			} catch(Exception e){
@@ -95,3 +132,4 @@ public class OnlineAuctionRegistrationServlet extends HttpServlet {
 		}
 	}
 }
+
